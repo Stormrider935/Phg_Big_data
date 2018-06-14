b9831f210820182ca34cf578b5e9c9dc0a70a72e
mike <stormrider935>
2018-06-14 15:03:22 +0200

    all acc numbers .fasta

--- get_sequences_by_accession.sh
+++ get_sequences_by_accession.sh
@@ -2,8 +2,7 @@ mkdir -p Phg_acc_meta_data_files
 while IFS= read -r acc; do
                 echo "Processing Accessionnumber: $acc"
                 efetch -db nuccore -id $acc -format fasta > Phg_acc_meta_data_files/$acc.fasta
-		efetch -db nuccore -id $acc -format gb > Phg_acc_meta_data_files/$acc.gb
+		        efetch -db nuccore -id $acc -format gb > Phg_acc_meta_data_files/$acc.gb
                 echo "Wrote $acc to file"
                 sleep "1"
         done < Phg_Big_data_acc_numbers
-
